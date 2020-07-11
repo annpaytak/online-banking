c7a4a14ae1f69946461202eceb688597155a12f5
annpaytak <anna.paytak@gmail.com>
2020-07-11 09:21:27 +0300

    server update

--- src/server/config/index.js
+++
@@ -1,2 +0,0 @@
-const dbURL = 'mongodb+srv://annamaria:b1a2n3k4i5n6g7@cluster0-z9znv.mongodb.net/clients';
-exports.dbURL = dbURL;
---
+++ src/server/models/user.model.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const User = mongoose.model(
+    "User",
+    new mongoose.Schema({
+        username: String,
+        email: String,
+        password: String,
+        roles: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Role"
+            }
+        ]
+    })
+);
+// roles is an array that contains ids in roles collection as reference.
+
+module.exports = User;
